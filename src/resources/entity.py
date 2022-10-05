86f040af4beb39d303b0f86804d4f6b5f21b3016
Anna Shtanova <as63@sanger.ac.uk>
2022-10-05 14:46:23 +0000

    Create scr directory

---
+++ src/resources/entity.py
@@ -0,0 +1,17 @@
+from Flask import request
+from flask_restful import Resource
+
+entities = []
+
+class Entity(Resource):
+    def get(self, id):
+        entity = next(filter(lambda item: item['id'] == id, entities), None)
+
+        return {'entity': entity}, 200 if entity else 404
+
+    def post(self, id):
+        data = request.get_json()
+        entity =  {'id': id, 'name': data['name']}
+        entities.append(entity)
+
+        return entity, 201
