b1595fda1b52b0f2d44228ba64179ff549a9595d
Anna Shtanova <as63@sanger.ac.uk>
2022-10-14 22:29:42 +0000

    TD-202 Create set of gRNAS, add forward sgRNA function

--- src/domain/guideRNA.py
+++ src/domain/guideRNA.py
@@ -1,5 +1,19 @@
+def create_set_of_gRNAs(data):
+    set_of_gRNAs = []
+
+    for line in data:
+        set_of_gRNAs.append(GuideRNA(line))
+
+    return set_of_gRNAs
+
 class GuideRNA:
+    FORWARD_PREFIX = "CACC"
+    REVERSE_PREFIX = "AAAC"
+
     def __init__(self, data) -> None :
         setattr(self, "id", data["id"])
         setattr(self, "sequence", data["sequence"])
         setattr(self, "gene_name", data["gene_name"])
+
+    def forward_sgRNA(self) -> str:
+        return self.FORWARD_PREFIX + getattr(self, "sequence")
