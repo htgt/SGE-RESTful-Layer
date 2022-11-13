9172de3409521248b384d3cfcc2409a137d627b5
Anna Shtanova <as63@sanger.ac.uk>
2022-11-13 23:48:48 +0000

    TD-232 Execute task

---
+++ src/domain/taskImportGrnas.py
@@ -0,0 +1,22 @@
+from src.benchling.get_blob import get_blob_url
+from src.benchling.guideRNA_from_csv import GrnasImportFromCSV
+
+class TaskImport:
+    def __init__(self, data):
+        self.id = data["id"]
+        self.file_id = data["file_id"]
+        self.status_id = data["status_id"]
+
+    def execute(self):
+        try:
+            file_url = get_blob_url(self.file_id)
+
+            importer = GrnasImportFromCSV()
+            importer.import_grnas(file_url)
+        except Exception as err:
+            return "Could not import guide RNAs", 500
+
+        return "Guide RNAs created", 200
+
+    def update_status(self, status):
+        pass
