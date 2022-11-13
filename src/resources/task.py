f97303189cf1da74bd1d20b65624e1e11b35beab
Anna Shtanova <as63@sanger.ac.uk>
2022-11-13 23:15:44 +0000

    TD-232 Transform json event data at the endpoint

--- src/resources/task.py
+++ src/resources/task.py
@@ -1,11 +1,61 @@
 from flask import request
 from flask_restful import Resource
 
+import json
+
 class TaskEndpoint(Resource):
     def get(self, id):
         return id, 201
 
-    def post(self):
-        data = request.json
 
-        return data, 201
+    def post(self):
+       # data = request.json
+
+        data = json.loads("""{
+            "id": "cda06ef4-eac1-9e84-8777-c8f9cad34605",
+            "detail-type": "v2.workflowTask.updated.status",
+            "detail": {
+                "workflowTask": {
+                    "id": "wftask_dMvTXfUn",
+                    "fields": {
+                        "gRNAs list CSV": {
+                            "type": "blob_link",
+                            "value": "773b18f1-1658-4865-aee3-f512a8d0e869"
+                        }
+                    },
+                    "schema": {
+                        "id": "prstsch_xlEsqy9T",
+                        "name": "SGE Import gRNAs"
+                    },
+                    "status": {
+                        "id": "wfts_DrqRAcOl",
+                        "statusType": "PENDING",
+                        "displayName": "Pending"
+                    },
+                    "workflowOutputs": [],
+                    "workflowTaskGroup": {
+                        "id": "prs_W2ENmFcb",
+                        "name": "SGE Import gRNAs 9",
+                        "displayId": "sge-grna9"
+                    }
+                },
+                "id": "evt_JmYcUA0O29oq",
+                "createdAt": "2022-11-07T13:19:46.375574+00:00",
+                "eventType": "v2.workflowTask.updated.status"
+            }
+        }""")
+
+        def transform_data(json_data):
+            result = {}
+            result["id"] = data["detail"]["workflowTask"]["id"]
+            result["file_id"] = data["detail"]["workflowTask"]["fields"]["gRNAs list CSV"]["value"]
+            result["status_id"] = data["detail"]["workflowTask"]["status"]["id"]
+
+            return result
+
+        if data["detail-type"] == "v2.workflowTask.updated.status" and data["detail"]["workflowTask"]["schema"]["id"] == "prstsch_xlEsqy9T" :
+            task_data = transform_data(data)
+
+            return task_data, 201
+        else:
+            return 404
