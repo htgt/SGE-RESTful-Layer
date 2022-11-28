188f5e27682ec3a88c5988aa8d34c0406195f25e
Anna Shtanova <as63@sanger.ac.uk>
2022-11-28 11:51:34 +0000

    TD-232 Use input data from incoming POST request

--- src/resources/task.py
+++ src/resources/task.py
@@ -6,6 +6,7 @@ import json
 from src.benchling.workflow_task import WorkflowTaskImport
 
 BENCHLING_WORKFLOW_TASK_SCHEMA_ID = "prstsch_bbPNDswA"
+BENCHLING_UPDATED_STATUS_EVENT = "v2.workflowTask.updated.status"
 
 class TaskEndpoint(Resource):
     def get(self, id):
@@ -14,79 +15,9 @@ class TaskEndpoint(Resource):
 
 
     def post(self):
-       # data = request.json
+        data = request.json
 
-        data = json.loads("""{
-            "version": "0",
-            "id": "70d32a26-20b2-81ae-fe94-5399384d269c",
-            "detail-type": "v2.workflowTask.updated.status",
-            "source": "aws.partner/benchling.com/tol-sangertest/sge-test",
-            "account": "110091099050",
-            "time": "2022-11-25T11:37:19Z",
-            "region": "us-east-1",
-            "resources": [],
-            "detail": {
-                "schema": {
-                "id": "prstsch_bbPNDswA",
-                "name": "SGE gRNA Auto Import"
-            },
-            "updates": [
-                "status"
-            ],
-            "deprecated": false,
-            "workflowTask": {
-                "id": "wftask_qOtMxQl3",
-                "fields": {
-                    "gRNAs list CSV": {
-                        "type": "blob_link",
-                        "value": "747881dc-85b6-477f-97d4-c0acdb56bcde",
-                        "isMulti": false,
-                        "textValue": "gRNA_example.csv",
-                        "displayValue": "gRNA_example.csv"
-                    }
-                },
-                "schema": {
-                    "id": "prstsch_bbPNDswA",
-                    "name": "SGE gRNA Auto Import"
-                },
-                "status": {
-                    "id": "wfts_EOjUQSei",
-                    "statusType": "IN_PROGRESS",
-                    "displayName": "In Progress"
-                },
-                "creator": {
-                    "id": "ent_wsSRluG6",
-                    "name": "Benchling Workflows",
-                    "handle": ""
-                },
-                "assignee": null,
-                "displayId": "sge-grna-auto2-T1",
-                "clonedFrom": null,
-                "scheduledOn": null,
-                "archiveRecord": null,
-                "executionType": "DIRECT",
-                "creationOrigin": {
-                    "originId": null,
-                    "originType": "PROCESSES_SCHEMA_FIELD_MAPPER",
-                    "application": "PROCESSES",
-                    "originModalUuid": null
-                },
-                "executionOrigin": null,
-                "workflowOutputs": [],
-                "workflowTaskGroup": {
-                    "id": "prs_97UJYcB0",
-                    "name": "SGE gRNA Auto Import 2",
-                    "displayId": "sge-grna-auto2"
-                }
-            },
-            "excludedProperties": [],
-            "id": "evt_aZriu5M5JM31",
-            "createdAt": "2022-11-25T11:37:19.157644+00:00",
-            "eventType": "v2.workflowTask.updated.status"
-            }
-        }""")
-
-        def transform_data(json_data):
+        def transform_data(data):
             task_data = {}
             task_data["id"] = data["detail"]["workflowTask"]["id"]
             task_data["file_id"] = data["detail"]["workflowTask"]["fields"]["gRNAs list CSV"]["value"]
@@ -94,7 +25,7 @@ class TaskEndpoint(Resource):
 
             return task_data
 
-        if data["detail-type"] == "v2.workflowTask.updated.status" and data["detail"]["workflowTask"]["schema"]["id"] == BENCHLING_WORKFLOW_TASK_SCHEMA_ID :
+        if data["detail-type"] == BENCHLING_UPDATED_STATUS_EVENT and data["detail"]["workflowTask"]["schema"]["id"] == BENCHLING_WORKFLOW_TASK_SCHEMA_ID :
 
             try:
                 task_data = transform_data(data)
