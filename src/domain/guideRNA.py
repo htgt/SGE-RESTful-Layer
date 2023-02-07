4ac6785202869599d084555a09d7e9d8506f1ae4
Anna Shtanova <as63@sanger.ac.uk>
2023-02-07 17:14:17 +0000

    TD-290 Add base change for Guide RNa Oligos: first base -> 'C', last base -> 'G'

--- src/domain/guideRNA.py
+++ src/domain/guideRNA.py
@@ -1,5 +1,12 @@
 from Bio.Seq import Seq
+from dataclasses import dataclass
 
+transformations_dict = {
+    "FORWARD_PREFIX": "CACC",
+    "REVERSE_PREFIX": "AAAC",
+    "FIRST_BASE": "G",
+    "LAST_BASE": "C",
+}
 
 def create_set_of_gRNAs(data):
     set_of_gRNAs = []
@@ -11,16 +18,42 @@ def create_set_of_gRNAs(data):
 
 
 class GuideRNA:
-    FORWARD_PREFIX = "CACC"
-    REVERSE_PREFIX = "AAAC"
-
     def __init__(self, data) -> None :
-        setattr(self, "id", data["wge_id"])
-        setattr(self, "sequence", Seq(data["seq"]))
-        setattr(self, "gene_name", data["gene_symbol"])
+        self.id = data["wge_id"]
+        self.sequence = Seq(data["seq"])
+        self.gene_name = data["gene_symbol"]
 
+        self.forward_prefix = transformations_dict["FORWARD_PREFIX"]
+        self.reverse_prefix = transformations_dict["REVERSE_PREFIX"]
+
+    # forward and reverse will not be used as a part of guideRNA class
     def forward_sgRNA(self) -> Seq:
-        return Seq(self.FORWARD_PREFIX + getattr(self, "sequence"))
+        return Seq(self.forward_prefix + self.sequence)
 
     def reverse_sgRNA(self) -> Seq:
-        return Seq(self.REVERSE_PREFIX + getattr(self, "sequence").reverse_complement())
+        return Seq(self.reverse_prefix + self.sequence.reverse_complement())
+
+
+@dataclass
+class OligosPair:
+    forward: str
+    reverse: str
+
+class GuideRNAOligo:
+    def __init__(self, seq) -> None:
+        self.sequence = Seq(seq)
+        self.first_base = transformations_dict["FIRST_BASE"]
+        self.last_base = transformations_dict["LAST_BASE"]
+        self.forward_prefix = transformations_dict["FORWARD_PREFIX"]
+        self.reverse_prefix = transformations_dict["REVERSE_PREFIX"]
+
+    def transform_first_and_last_bases(self) -> str:
+        return self.first_base + self.sequence[1:-1] + self.last_base
+
+    def create_oligos(self) -> OligosPair:
+        transformed_seq = self.transform_first_and_last_bases()
+
+        return OligosPair(
+            self.forward_prefix + transformed_seq,
+            self.reverse_prefix + transformed_seq.reverse_complement()
+        )
