b341294507e21fc0084b9bbb7ce742560c2a0c98
pk8 <pk8@sanger.ac.uk>
2023-03-08 23:28:06 +0000

    Added test for as_entity

---
+++ tests/benchling/test_create_targeton_oligos.py
@@ -0,0 +1,98 @@
+import unittest
+import json
+
+from unittest.mock import patch
+from src.domain.targeton_oligos import TargetonOligo
+from src.benchling import benchling_connection
+from src.benchling.targeton_oligos import (
+    as_benchling_entity,
+)
+
+
+class TestCreateOligo(unittest.TestCase):
+    def setUp(self):
+        self.data = {
+            'entity_name': 'chr17_43104794_43105038_minus_sgRNA_ex4',
+            'chromosome': 'chr17',
+            'strand': '-',
+            'start': '43104794',
+            'end': '43105038',
+            'r2_start': '43104868',
+            'r2_end': '43104956',
+            'ext_vector': '25,25',
+            'action_vector': '(1del,2del1,snv),(1del,snvre,inframe,stop,ala),(1del,2del0,snv)',
+            'sgrna_vector': 'sgRNA_ex4'
+        }
+        self.targeton_oligo = TargetonOligo(self.data)
+        self.packet = {
+            'name': 'chr17_43104794_43105038_minus_sgRNA_ex4',
+            'folderId': 'lib_MaKCkHDE',
+            'schemaId': 'ts_UP3gzN10',
+            'fields': {
+                'Action Vector': {
+                    'value': '(1del,2del1,snv),(1del,snvre,inframe,stop,ala),(1del,2del0,snv)'
+                },
+                'Ext Vector': {
+                    'value': '25,25'
+                },
+                'Ref. Start Position': {
+                    'value': 43104794
+                },
+                'Ref. End Position': {
+                    'value': 43105038
+                },
+                'Ref2. Start Position': {
+                    'value': 43104868
+                },
+                'Ref2. End Position': {
+                    'value': 43104956
+                },
+                'Reference Chromosome': {
+                    'value': 'sfso_yMb5PNqX'
+                },
+                'Reference Strand': {
+                    'value': 'sfso_qKNl7o1M'
+                },
+                'sgRNA Vector': {
+                    'value': 'sgRNA_ex4'
+                }
+            }
+        }
+
+    def test_as_benchling_entity(self):
+        expected_response = {
+            'Action Vector': {
+                'value': '(1del,2del1,snv),(1del,snvre,inframe,stop,ala),(1del,2del0,snv)'
+            },
+            'Ext Vector': {
+                'value': '25,25'
+            },
+            'Ref. Start Position': {
+                'value': 43104794
+            },
+            'Ref. End Position': {
+                'value': 43105038
+            },
+            'Ref2. Start Position': {
+                'value': 43104868
+            },
+            'Ref2. End Position': {
+                'value': 43104956
+            },
+            'Reference Chromosome': {
+                'value': 'sfso_yMb5PNqX'
+            },
+            'Reference Strand': {
+                'value': 'sfso_qKNl7o1M'
+            },
+            'sgRNA Vector': {
+                'value': 'sgRNA_ex4'
+            }
+        }
+
+        actual_response = as_benchling_entity(self.targeton_oligo)
+
+        self.assertDictEqual(actual_response, expected_response)
+
+if __name__ == '__main__':
+    unittest.main()
