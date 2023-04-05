06745c7c500a252257dd8d6cad41ba1b1fa4c0fe
Beth Sampher <bs15@sanger.ac.uk>
2023-04-05 11:44:56 +0100

    TD-359 Separate PAM from guide RNA
    Also removed some unused files

--- src/resources/blob.py
+++ src/resources/blob.py
@@ -2,13 +2,10 @@ from flask_restful import Resource
 import json
 
 from src.benchling.get_blob import get_blob_url
-from src.benchling.guideRNA_from_csv import GrnasImportFromCSV
 
 
 class Blob(Resource):
     def get(self, id):
-        csv_url = get_blob_url(id)
+        url = get_blob_url(id)
 
-        result = GrnasImportFromCSV().get_grnas(csv_url)
-
-        return result, 201
+        return 201
