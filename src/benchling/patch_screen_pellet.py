a514d52f2b44b615ea305a0c0ff4bd3801a6a33c
Anna Shtanova <as63@sanger.ac.uk>
2023-04-13 16:15:01 +0000

    TD-361 Fix type annotation

--- src/benchling/patch_screen_pellet.py
+++ src/benchling/patch_screen_pellet.py
@@ -4,20 +4,20 @@ from . import benchling_connection, benchling_schema_ids
 from src.benchling.utils.export_to_benchling import export_to_benchling_json_response
 
 
-def patch_screen_pellet(pellet_data: json, pellet_id: str) -> str:
+def patch_screen_pellet(pellet_data: dict, pellet_id: str) -> dict:
     benchling_body = as_benchling_req_body(pellet_data)
     patch_url = benchling_connection.custom_entity_url + '/' + pellet_id
 
-    response = export_to_benchling_json_response(
+    json_response = export_to_benchling_json_response(
         benchling_body,
         patch_url,
         benchling_connection,
         'patch',
     )
 
-    return response
+    return json_response
 
-def as_benchling_req_body(pellet_data: json) -> dict:
+def as_benchling_req_body(pellet_data: dict) -> dict:
     schema_id = benchling_schema_ids.ids['schemas']['screen_pellet_schema_id']
     status = benchling_schema_ids.ids['dropdowns']['sequencing_status'][pellet_data['run_status']]
 
