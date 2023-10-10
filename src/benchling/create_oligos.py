1be0366b9b5684b0716c217d72217569a4d39214
Anna Shtanova <as63@sanger.ac.uk>
2023-10-10 14:52:15 +0000

    TD-411 Print out requests

--- src/benchling/create_oligos.py
+++ src/benchling/create_oligos.py
@@ -60,6 +60,11 @@ def export_oligos_to_benchling(oligos: BenchlingOligosPair, url: str) -> Tuple[s
     oligo_forward_json = prepare_oligo_json(oligos.forward)
     oligo_reverse_json = prepare_oligo_json(oligos.reverse)
 
+    print('EXPORT Oligos to Benchling')
+    print(url)
+    print(oligo_forward_json)
+    print(oligo_reverse_json)
+
     oligo_forward = request_to_benchling_json_response(
         url,
         'post',
