195d85e053a223027b27c67a0e1c2d1ac179ca57
Anna Shtanova <as63@sanger.ac.uk>
2023-10-12 20:00:12 +0000

    TD-411 Move get wge data

--- src/core/guide.py
+++ src/core/guide.py
@@ -11,7 +11,11 @@ from src.benchling import benchling_schema_ids
 def handle_guide_event(data : dict) -> dict:
     response = {}
     try:
-        response['grna'] = patch_grna_event(data)
+        print(data['fields']['WGE ID']['value'])
+        wge_response = query_wge_by_id(data['fields']['WGE ID']['value'])
+        print('WGE response', wge_response)
+
+        response['grna'] = patch_grna_event(data, wge_response)
         print('GRNA patched')
     except Exception as err:
         return str(err), 500
@@ -24,15 +28,13 @@ def handle_guide_event(data : dict) -> dict:
         return str(err), 500
 
 
-def patch_grna_event(data : dict) -> dict:
-    if check_wge_id(data):
-        wge_event = transform_wge_event(data)
-        response = patch_wge_data_to_service(wge_event)
+def patch_grna_event(data : dict, wge_response: dict) -> dict:
+    wge_event = transform_wge_event(data)
+    response = patch_wge_data_to_service(wge_event, wge_response)
 
-        return response
+    return response
 
-def patch_wge_data_to_service(event_data : dict) -> dict:
-    wge_data = query_wge_by_id(event_data['wge_id'])
+def patch_wge_data_to_service(event_data : dict, wge_data: dict) -> dict:
     grna_object = prepare_guide_rna_class(event_data, wge_data)
 
     response = patch_guide_rna(grna_object, event_data)
